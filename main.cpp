dd541eac06bef02eee9500d4f48acfa5c296ed9d
BayernMuller <cjjunk29@gmail.com>
2024-02-27 09:52:03 +0900

    refactor: Update main.cpp and static/index.html

--- main.cpp
+++ main.cpp
@@ -9,25 +9,21 @@ response index(request req)
 	if (req.method == "GET")
 	{
 		HtmlRender render;
-		string title = "BayernMuller";
-		string comment = "I am a developer";
+		string title = "Hello! I'm Jayden.";
+		string comment = "Bayern Munich is the best";
+		string email = "cjjunk29@gmail.com";
 		render.Insert(title);
 		render.Insert(comment);
+		render.Insert(email);
 		return { 200, render("static/index.html") };
 	}
 	return { 400, "Not allowed method" };
 }
 
-response button(request req)
-{
-	HtmlRender render;
-	return { 200, render("static/button.html") };
-}
-
 int main() 
 {
-	Libero server("0.0.0.0", 8000);
+	Libero server("0.0.0.0", 8500);
 	server.Route("/", index);
-	server.Route("/button", button);
 	server.Run();
 }
+

a2a12c7472dfd6620f554580d60ce3cad82a0f8f
BayernMuller <cjjunk29@gmail.com>
2024-03-13 18:36:01 +0900

    feat: updated HtmlRender implementation with file_path handling

--- libero/htmlrender.cpp
+++ libero/htmlrender.cpp
@@ -2,5 +2,42 @@
 
 namespace libero
 {
-	
+    HtmlRender::HtmlRender(const char* file_path)
+        : mFilePath(file_path)
+    {
+    }
+    
+	std::string HtmlRender::operator()(const char* html)
+	{
+		std::ifstream file(html);
+		std::string str;
+		str.reserve(2048);
+
+		if (!file.is_open() || !file.good())
+		{
+			throw std::runtime_error("file open error");
+		}
+
+		std::istreambuf_iterator<char> iter(file), end;
+		str.assign(iter, end);
+
+		std::size_t begin_token = 0;
+		while ((begin_token = str.find("{{", begin_token)) != std::string::npos)
+		{
+			std::size_t end_token = str.find("}}", begin_token);
+			std::string key = str.substr(begin_token + 2, end_token - begin_token - 2);
+			str.replace(begin_token, end_token - begin_token + 2, mValues[key]);
+			begin_token = end_token + 2;
+		}
+		return str;
+	}
+
+    HtmlRender::operator std::string()
+    {
+		if (mFilePath.empty())
+		{
+			throw std::runtime_error("file path is empty");
+		}
+        return (*this)(mFilePath.c_str());
+    }
 }

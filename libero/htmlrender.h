deba3d662533321bd8da2bda938d22a303be1fcf
BayernMuller <cjjunk29@gmail.com>
2024-03-13 18:36:01 +0900

    feat: updated HtmlRender header with redesigned methods

--- libero/htmlrender.h
+++ libero/htmlrender.h
@@ -10,13 +10,18 @@ namespace libero
 	class HtmlRender
 	{
 	public:
+		HtmlRender() = default;
+		HtmlRender(const char* file_path);
+		~HtmlRender() = default;
+
+		operator std::string();
+		std::string operator()(const char* file_path);
+
 		template<class T>
 		void Put(const char* name, T&& value);
 
-		template<int N>
-		std::string operator()(const char(&html)[N]);
-
 	private:
+		std::string mFilePath;
 		std::map<std::string, std::string> mValues;
 	};
 
@@ -26,32 +31,6 @@ namespace libero
 		std::ostringstream oss;
 		oss << value;
 		mValues[name] = oss.str();
-	}
-
-	template<int N>
-	std::string HtmlRender::operator()(const char(&html)[N])
-	{
-		std::ifstream file(html);
-		std::string str;
-		str.reserve(2048);
-
-		if (!file.is_open() || !file.good())
-		{
-			throw std::runtime_error("file open error");
-		}
-
-		std::istreambuf_iterator<char> iter(file), end;
-		str.assign(iter, end);
-
-		std::size_t begin_token = 0;
-		while ((begin_token = str.find("{{", begin_token)) != std::string::npos)
-		{
-			std::size_t end_token = str.find("}}", begin_token);
-			std::string key = str.substr(begin_token + 2, end_token - begin_token - 2);
-			str.replace(begin_token, end_token - begin_token + 2, mValues[key]);
-			begin_token = end_token + 2;
-		}
-		return str;
 	}
 }
 
